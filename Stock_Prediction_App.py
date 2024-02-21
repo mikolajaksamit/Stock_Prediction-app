94eb4523a885432cb7d39919864ed6798b195d2d
mikolajaksamit <157887790+mikolajaksamit@users.noreply.github.com>
2024-02-21 14:03:49 +0100

    Update Stock_Prediction_App.py

--- Stock_Prediction_App.py
+++ Stock_Prediction_App.py
@@ -3,7 +3,7 @@ from datetime import date
 import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
-
+import ta
 from plotly import graph_objects as go
 
 START = "2015-01-01"
