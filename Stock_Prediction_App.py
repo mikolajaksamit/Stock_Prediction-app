8ad96969c7326b0b5b0fab6a6f9eca7eb93a3ccf
mikolajaksamit <157887790+mikolajaksamit@users.noreply.github.com>
2024-02-21 23:02:35 +0100

    Update Stock_Prediction_App.py

--- Stock_Prediction_App.py
+++ Stock_Prediction_App.py
@@ -4,7 +4,7 @@ import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objects as go
-
+import requests
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
