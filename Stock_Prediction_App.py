d5b0d9f123c8ee50dfee7180d608ab4ad4f74f66
mikolajaksamit <157887790+mikolajaksamit@users.noreply.github.com>
2025-02-18 13:25:32 +0100

    Update Stock_Prediction_App.py

--- Stock_Prediction_App.py
+++ Stock_Prediction_App.py
@@ -1,56 +1,50 @@
 import streamlit as st
 from datetime import date
 import yfinance as yf
+import pandas as pd
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objects as go
-import requests
+
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title("Stock Prediction App")
 
 stocks = ("AAPL", "META", "GOOG", "MSFT", "BTC-USD", "ETH-USD")
-
 selected_stock = st.selectbox("Select dataset for prediction", stocks)
 
 n_years = st.slider("Year of prediction", 1, 9)
 period = n_years * 365
 
-
 def load_data(ticker):
     data = yf.download(ticker, start=START, end=TODAY)
     data.reset_index(inplace=True)
     return data
-data_load_state = st.text("Load data...")
+
+data_load_state = st.text("Loading data...")
 data = load_data(selected_stock)
 data_load_state.text("Loading data complete!")
 
 st.subheader("Raw data")
 st.write(data.tail())
 
-
 def calculate_sma(data, window):
-    sma = data['Close'].rolling(window=window).mean()
-    return sma
+    return data['Close'].rolling(window=window).mean()
+
 def calculate_rsi(data, window):
     delta = data['Close'].diff()
     gain = (delta.where(delta > 0, 0)).rolling(window=window).mean()
     loss = (-delta.where(delta < 0, 0)).rolling(window=window).mean()
     rs = gain / loss
-    rsi = 100 - (100 / (1 + rs))
-    return rsi
+    return 100 - (100 / (1 + rs))
 
-
-# Obliczanie SMA dla 20 okresów
+# Technical Indicators
 data['SMA'] = calculate_sma(data, window=20)
-
-# Obliczanie RSI dla 14 okresów
 data['RSI'] = calculate_rsi(data, window=14)
 
 st.subheader("Data with Technical Indicators")
 st.write(data.tail())
-
 
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=data["Date"], y=data["Close"], name="Close", line=dict(color="blue")))
@@ -63,11 +57,15 @@ fig_rsi = go.Figure()
 fig_rsi.add_trace(go.Scatter(x=data["Date"], y=data["RSI"], name="RSI", line=dict(color="green")))
 fig_rsi.layout.update(title_text="RSI (14)", xaxis_rangeslider_visible=True)
 st.plotly_chart(fig_rsi)
-#forecasting
 
-df_train = data[["Date", "Close"]]
+# Forecasting
+df_train = data[["Date", "Close"]].copy()
 df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+df_train["ds"] = pd.to_datetime(df_train["ds"])
+df_train["y"] = pd.to_numeric(df_train["y"], errors="coerce")
+df_train = df_train.dropna()
 
+# Model training
 m = Prophet()
 m.fit(df_train)
 
@@ -77,12 +75,10 @@ forecast = m.predict(future)
 st.subheader("Forecast data")
 st.write(forecast.tail())
 
-st.write('Forecast data')
+st.write('Forecast plot')
 fig1 = plot_plotly(m, forecast)
 st.plotly_chart(fig1)
 
 st.write('Forecast components')
 fig2 = m.plot_components(forecast)
 st.write(fig2)
-
-
